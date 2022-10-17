7501107839c444b6c6ffd36d6750e1020ed47943
LazaroBlink <jimenesmoncho@gmail.com>
2022-10-16 19:52:59 -0600

    Add code ti class App

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,6 @@
 import 'dotenv/config';
-import express from 'express';
+import App from './app';
 
-const PORT = process.env.APP_PORT || 3001;
+//Iniciar la Aplicacion
 
-const app = express();
-
-app.listen(PORT, () => {
-    console.log(`Server running in port: ${PORT}`);
-});
+App.listen(process.env.APP_PORT as unknown as number | 3001); 
