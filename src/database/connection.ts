ed8195214384ae78c3d1408916c2f4213389a897
LazaroBlink <jimenesmoncho@gmail.com>
2022-10-24 16:01:29 -0600

    Conexion a postgres terminada

---
+++ src/database/connection.ts
@@ -0,0 +1,24 @@
+import { Sequelize } from 'sequelize-typescript';
+import * as dotenv from 'dotenv'
+
+class Connection {
+
+    public connection: Sequelize
+
+    constructor() {
+        dotenv.config();
+
+        this.connection = new Sequelize({
+            dialect: "postgres",
+            host: process.env.BD_HOST,
+            username: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+           // port: 5432,
+            logging: false
+        });
+        
+    }
+}
+
+export default Connection;
